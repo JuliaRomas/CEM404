d86f1bc245d69c52e7a65bb0f6dafd94796f74a0
Julia <juliaromas2007@gmail.com>
2024-11-28 11:50:02 -0300

    adição de novas coisas

---
+++ script.js
@@ -0,0 +1,11 @@
+
+window.sr=ScrollReveal({reset:true});
+
+
+sr.reveal('h1',{rotate:{x:0,y:80,z:0},duration:2000});
+sr.reveal('h4',{rotate:{x:0,y:80,z:0},duration:2000});
+sr.reveal('h5',{rotate:{x:0,y:80,z:0},duration:2000});
+sr.reveal('h6',{rotate:{x:0,y:80,z:0},duration:2000});
+sr.reveal('h7',{rotate:{x:0,y:80,z:0},duration:2000});
+sr.reveal('p',{rotate:{x:0,y:80,z:0},duration:2000});
+sr.reveal('img',{rotate:{x:0,y:80,z:0},duration:2000});
